eb265961d4300e7215971be864eb474661e01bad
Sergey Ermakov <seermakov355@yandex.ru>
2021-09-15 12:03:10 +0300

    added

--- src/ru/mirea/test/Main.java
+++ src/ru/mirea/test/Main.java
@@ -7,3 +7,30 @@ public class Main {
         System.out.print("Hi");
     }
 }
+
+//https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
+//http://cpo-alex.blogspot.com/p/intellij-idea-cannot-resolve-simbol.html
+//https://habr.com/ru/post/195674/
+
+//сервер
+//оценки и защита работ
+
+//формирование документов
+//typora
+
+/*
+регистрация на гитхабе
+изменение имени
+форк репозитория
+
+ноуты:
+авторизация в интелиджи
+клон репозитория
+
+компы:
+создания токена
+открытие репозитория через ссылку
+
+вставка работ
+
+ */
