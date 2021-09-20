07e75470ef4b043fe2b5a5f815bcdfe427a9911d
vs git <test@email.com>
2021-09-20 20:02:52 +0300

    +task3.exe1

---
+++ src/ru/mirea/task3/exe1/Circle.java
@@ -0,0 +1,39 @@
+package ru.mirea.task3.exe1;
+
+public class Circle {
+    private double rad;
+    private double square;
+    private double length;
+
+    Circle(double rad){
+        this.rad = rad;
+    }
+
+    public double getRad(){
+        return rad;
+    }
+
+    public double getSquare(){
+        return square;
+    }
+
+    public void setRad(double rad){
+        this.rad = rad;
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public String toString(){
+        return "Radius: " + rad + " Length: " + length + " Square: " + square;
+    }
+
+    public void calcSquare(){
+        square = Math.atan(1) * 4 * rad * rad;
+    }
+
+    public void calcLength(){
+        length = Math.atan(1) * 4 * rad * 2;
+    }
+}
