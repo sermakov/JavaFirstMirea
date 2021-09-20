87513b2ed5759e76bdcbfbb46f1ec09ed30704cf
vs git <test@email.com>
2021-09-20 20:30:14 +0300

    +task3.exe3

---
+++ src/ru/mirea/task5/exe1/Saucer.java
@@ -0,0 +1,34 @@
+package ru.mirea.task5.exe1;
+
+public class Saucer extends Dish
+{
+    private double dishWeight;
+    private String dishShape;
+    private String colour;
+
+    Saucer(double dishWeight, String dishShape, String colour)
+    {
+        this.dishWeight = dishWeight;
+        this.dishShape = dishShape;
+        this.colour = colour;
+    }
+
+    public void setColour(String colour){
+        this.colour = colour;
+    }
+
+    @Override
+    public void setDishShape(String dishShape){
+        this.dishShape = dishShape;
+    }
+    @Override
+    public void setDishWeight(double dishWeight){
+        this.dishWeight = dishWeight;
+    }
+    @Override
+    public String getDishShape() { return dishShape;}
+    @Override
+    public double getDishWeight() {return dishWeight;}
+    public String DishColour() { return colour;}
+}
+
---
+++ src/ru/mirea/task5/exe1/TestDish.java
@@ -0,0 +1,23 @@
+package ru.mirea.task5.exe1;
+
+public class TestDish {
+
+    public static void main(String[] args) {
+        Plate plate = new Plate(0.8, "square", "white");
+        Jug jug = new Jug(2.1, "circle", "red");
+        Saucer saucer = new Saucer(1.5, "circle", "blue");
+
+        System.out.println("Plate" + "\t" + plate.getDishShape() + " " + plate.getDishWeight());
+        System.out.println("Jug" + "\t" + jug.getDishShape() + " " + jug.getDishWeight());
+        System.out.println("Saucer" + "\t" + saucer.getDishShape() + " " + saucer.getDishWeight());
+
+        plate.setDishWeight(2.5); plate.setDishShape("circle");
+        saucer.setDishWeight(2.6);
+
+        System.out.println( "\n" + "Plate" + "\t" + plate.getDishShape() + " " + plate.getDishWeight());
+        System.out.println("Jug" + "\t" + jug.getDishShape() + " " + jug.getDishWeight());
+        System.out.println("Saucer" + "\t" + saucer.getDishShape() + " " + saucer.getDishWeight());
+
+
+    }
+}
