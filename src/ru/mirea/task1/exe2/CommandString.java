3dfcd7960f40d02b7a5acc189cdf6554de72529b
vs git <test@email.com>
2021-09-20 19:16:22 +0300

    +task1.exe2

---
+++ src/ru/mirea/task1/exe2/CommandString.java
@@ -0,0 +1,8 @@
+package ru.mirea.task1.exe2;
+
+public class CommandString {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; ++i)
+            System.out.println(args[i]);
+    }
+}
