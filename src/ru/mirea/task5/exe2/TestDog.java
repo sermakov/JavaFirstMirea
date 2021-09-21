29fa9afa1856fe919ad22cecb1d320c83fa56ce1
vs git <test@email.com>
2021-09-21 18:39:06 +0300

    +task5exe2

--- src/ru/mirea/task5/exe2/TestDog.java
+++ src/ru/mirea/task5/exe2/TestDog.java
@@ -2,9 +2,9 @@ package ru.mirea.task5.exe2;
 
 public class TestDog {
     public static void main(String[] args) {
-        Dobermann dobermann = new Dobermann(32, 0.68, "white");
-        Pitbull pitbull = new Pitbull(26, 0.53, "red");
-        Rottweiler rottweiler = new Rottweiler(50, 0.6, "blue");
+        Dog dobermann = new Dobermann(32, 0.68, "white");
+        Dog pitbull = new Pitbull(26, 0.53, "red");
+        Dog rottweiler = new Rottweiler(50, 0.6, "blue");
 
         System.out.println("Dobermann" + "\t" + dobermann.getDogHeight() + " " + dobermann.getDogWeight());
         System.out.println("Pitbull" + "\t" + pitbull.getDogHeight() + " " + pitbull.getDogWeight());
