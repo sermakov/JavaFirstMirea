a30e2841b8c728e02f2f70ad44dc89cacc13e5e8
vs git <test@email.com>
2021-09-21 17:55:13 +0300

    +task2exe1

--- src/ru/mirea/task2/exe1/TestShape.java
+++ src/ru/mirea/task2/exe1/TestShape.java
@@ -2,7 +2,7 @@ package ru.mirea.task2.exe1;
 
 public class TestShape {
     public static void main(String[] args){
-        Shape shape = new Shape(40, "circle");
+        Shape shape = new Shape(41, "circle");
         System.out.println(shape);
         shape.setSize(60);
         System.out.println("size " + shape.getSize() + " " + shape.getShape());
