b08beebaabad457ad253ed5f0938a317234e85fe
vs git <test@email.com>
2021-09-21 17:54:27 +0300

    +task2exe2

--- src/ru/mirea/task2/exe2/TestBall.java
+++ src/ru/mirea/task2/exe2/TestBall.java
@@ -2,7 +2,7 @@ package ru.mirea.task2.exe2;
 
 public class TestBall {
     public static void main(String[] args){
-        Ball ball = new Ball(40, "circle", "red");
+        Ball ball = new Ball(41, "circle", "red");
         System.out.println(ball.toString());
         System.out.println("\n" + ball.getSize() + " " + ball.getShape() + " " + ball.getColor());
     }
