718cac7d75418154f7c6615156a445f7f7edd144
vs git <test@email.com>
2021-09-21 17:49:20 +0300

    +

--- src/ru/mirea/task2/exe1/TestShape.java
+++ src/ru/mirea/task2/exe1/TestShape.java
@@ -3,9 +3,10 @@ package ru.mirea.task2.exe1;
 public class TestShape {
     public static void main(String[] args){
         Shape shape = new Shape(40, "circle");
-        System.out.println(shape.toString());
+        System.out.println(shape);
         shape.setSize(60);
+        System.out.println("size " + shape.getSize() + " " + shape.getShape());
         shape.setShape("square");
-        System.out.println("\n" + "size " + shape.getSize() + " " + shape.getShape());
+        System.out.println("size " + shape.getSize() + " " + shape.getShape());
     }
 }
