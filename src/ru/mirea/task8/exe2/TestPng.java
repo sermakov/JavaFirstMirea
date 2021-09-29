b537676203c046d98bfd5428c65b51febc43a1f8
vs git <test@email.com>
2021-09-29 14:24:37 +0300

    +task8exe2

--- src/ru/mirea/task8/exe2/TestPng.java
+++ src/ru/mirea/task8/exe2/TestPng.java
@@ -1,56 +1,50 @@
 package ru.mirea.task8.exe2;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-public class TestPng {
+import java.io.IOException;
+import java.io.File;
+
+public class TestPng extends Application {
+    private static String fileName = "";
+
     public static void main(String[] args) {
-        final int xSize = 400;
-        final int ySize = 280;
-        final Color backgroundColor = Color.WHITE;
-        final String text = "SQL Browser";
-        final String version = "Product Version: 1.0";
-
-        final Stage aboutDialog = new Stage();
-        aboutDialog.initModality(Modality.WINDOW_MODAL);
-
-        Button closeButton = new Button("Close");
-
-        closeButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent arg0) {
-                aboutDialog.close();
+        if(args.length > 0)
+            fileName = args[0];
+        if(fileName == "") {
+            File f = new File(fileName);
+            if(!f.exists() || f.isDirectory()) {
+                System.out.println("File not exist!");
+                System.out.println("Press any key to exit.");
+                //System.in.read();
+                System.exit(0);
             }
-        });
+        }
+        Application.launch(args);
+    }
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+    @Override
+    public void start(Stage stage) throws Exception {
+        Image image = new Image(fileName);
 
-        Image img = new Image("file:image.png");
-        ImageView imgView = new ImageView(img);
+        ImageView imageView = new ImageView(image);
+        imageView.setX(50);
+        imageView.setY(25);
 
-        grid.add(imgView, 0, 0);
+        imageView.setFitHeight(500);
+        imageView.setFitWidth(500);
 
-        grid.add(new Text(text), 0, 1);
-        grid.add(new Text(version), 0, 2);
-        grid.add(closeButton, 14, 18);
+        imageView.setPreserveRatio(true);
 
-        Scene aboutDialogScene = new Scene(grid, xSize, ySize, backgroundColor);
-        aboutDialog.setScene(aboutDialogScene);
-        aboutDialog.show();
+        Group root = new Group(imageView);
+        Scene scene = new Scene(root, 601, 500);
+        stage.setTitle("Image view");
+        stage.setScene(scene);
+        stage.show();
     }
 }
