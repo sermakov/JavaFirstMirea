f535e60cbd9c5ee616961448d3bc3a3a35a67157
Eugene Gorbas <evgengorbasiy2002@gmail.com>
2021-11-11 00:33:48 +0300

    kfdkfkdljdskl

---
+++ src/ru/mirea/task2/Ball.java
@@ -0,0 +1,10 @@
+public class Ball
+{
+    int radius = 8;
+
+    @Override
+    public String toString() {
+        return "Ball: " +
+                "\nradius=" + radius;
+    }
+}
---
+++ src/ru/mirea/task2/TestB.java
@@ -0,0 +1,9 @@
+public class TestB
+{
+    public static void main(String[] args)
+    {
+        Ball b1 = new Ball();
+
+        System.out.println(b1);
+    }
+}
