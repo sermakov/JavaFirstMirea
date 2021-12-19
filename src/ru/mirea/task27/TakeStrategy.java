25924628b7b088dcca3334700a7dfcc9fc684b99
Kvadron <anlimirea@yandex.ru>
2021-12-19 15:20:18 +0300

    Task 27 Done

---
+++ src/ru/mirea/task27/TakeStrategy.java
@@ -0,0 +1,6 @@
+package ru.mirea.task27;
+
+public interface TakeStrategy
+{
+    boolean take();
+}
---
+++ src/ru/mirea/task27/Takeable.java
@@ -0,0 +1,11 @@
+package ru.mirea.task27;
+
+import java.util.HashMap;
+
+public class Takeable extends ItemDecorator implements Item
+{
+    public Takeable(String name, HashMap<String, Integer> iactResp)
+    {
+        super(new Prop(name, iactResp), type.T);
+    }
+}
